c2ff078d245dd07206b81265afdeba8ab1aadc66
金田尚大 <1111510078r21@gmail.com>
2019-10-23 07:46:20 +0900

    storeId指定のルートを追加

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers;
 
 class StoreController extends Controller
 {
+  /**
+   * @return Response
+   */
   public function getAll()
   {
     return 'hello';
+  }
+
+  /**
+   * @param int $storeId
+   * @return Response
+   */
+  public function getByStoreId(int $storeId)
+  {
+    return 'storeId : ' . $storeId;
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
 <?php
 
 $router->get('/api/' . env('VERSION') . '/stores', 'StoreController@getAll');
+$router->get('/api/' . env('VERSION') . '/store/{storeId}', 'StoreController@getByStoreId');
