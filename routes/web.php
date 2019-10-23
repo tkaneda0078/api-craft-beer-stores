4477823efa14421f094fb5dde4a6838e041675e8
金田尚大 <1111510078r21@gmail.com>
2019-10-23 22:53:58 +0900

    ruteをgroup化

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
-$router->get('/api/' . env('VERSION') . '/stores', 'StoreController@getAll');
-$router->get('/api/' . env('VERSION') . '/store/{storeId}', 'StoreController@getByStoreId');
-$router->post('/api/' . env('VERSION') . '/store', 'StoreController@store');
+$router->group(['prefix' => '/api/' . env('VERSION')], function () use ($router) {
+  $router->get('/stores', 'StoreController@getAll');
+  $router->get('/store/{storeId}', 'StoreController@getByStoreId');
+  $router->post('/store', 'StoreController@store');
+});
